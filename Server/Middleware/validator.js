261ebb2ade160c996139fbd5649a2591fb236bd7
Iyara Ferguson <fergusoniyara@gmail.com>
2019-04-12 15:19:42 +0100

    feature(create-bank-account):create endpoint that enables user create an account  [Starts #165312109]

--- Server/Middleware/validator.js
+++ Server/Middleware/validator.js
@@ -3,16 +3,25 @@
 import Joi from 'joi';
 import testData from '../data/testData';
 
+class Validator {
+  static validateSignUpInput(user) {
+    const schema = {
+      firstName: Joi.string().required().error(new Error('First Name is required')),
+      lastName: Joi.string().required().error(new Error('Last Name is required')),
+      email: Joi.string().email().required().error(new Error('Your Email is required, example fergusoniyara@banka.com')),
+      password: Joi.string().regex(/^\S+$/).required().min(4)
+        .error(new Error('Password should be at least 4 characters without any whitespace(s)')),
+    };
+    return Joi.validate(user, schema);
+  }
 
-const validateSignUpInput = (user) => {
-  const schema = {
-    firstName: Joi.string().required().error(new Error('First Name is required')),
-    lastName: Joi.string().required().error(new Error('Last Name is required')),
-    email: Joi.string().email().required().error(new Error('Your Email is required, example fergusoniyara@banka.com')),
-    password: Joi.string().regex(/^\S+$/).required().min(4)
-      .error(new Error('Password should be at least 4 characters without any whitespace(s)')),
-  };
-  return Joi.validate(user, schema);
-};
+  static validateCreateAccount(user) {
+    const schema = {
+      type: Joi.string().required().error(new Error('Enter An Account Type Please')),
+    };
 
-export default validateSignUpInput;
+    return Joi.validate(user, schema);
+  }
+}
+
+export default Validator;
--- Server/helper/helper.js
+++ Server/helper/helper.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 // eslint-disable-next-line no-unused-vars
 import testData from '../data/testData';
 
-
+const { accounts } = testData;
 class Helper {
   static findUserByEmail(users) {
     return users.reduce((emailArray, userDetail) => emailArray.concat(userDetail.email), []);
@@ -18,6 +18,10 @@ class Helper {
     );
 
     return token;
+  }
+
+  static createAccountNumber() {
+    return 200000001 + accounts.length;
   }
 }
 
