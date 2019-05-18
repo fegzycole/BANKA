c7a6a4db91c55f6a3bf2254f35904882bda940b3
Iyara Ferguson <fergusoniyara@gmail.com>
2019-05-18 23:01:25 +0100

    Rearrange gh-pages

--- js/createaccount.js
+++ js/createaccount.js
@@ -1,12 +1,12 @@
-myForm.addEventListener('submit',validate);
-function validate(e){
-    successForm.style.display        = 'block';
-    userAccountNameTextBox.value     = '';
-    userPhoneNoTextBox.value         = '';
-    userEmailTextBox.value           = '';
-    selectBox.value                  = '';
-    setInterval(()=>{
-      successForm.style.display      = 'none';
-    },3000);
-e.preventDefault();
-}
+const validate = (e) => {
+  successForm.style.display = 'block';
+  userAccountNameTextBox.value = '';
+  userPhoneNoTextBox.value = '';
+  userEmailTextBox.value = '';
+  selectBox.value = '';
+  setInterval(() => {
+    successForm.style.display = 'none';
+  }, 3000);
+  e.preventDefault();
+};
+myForm.addEventListener('submit', validate);
--- js/signin.js
+++ js/signin.js
@@ -1,18 +1,30 @@
-/* eslint-disable no-unused-vars */
 const userEmailTextBox = document.getElementById('userEmailTextBox');
+
 const userPasswordBox = document.getElementById('userPasswordTextBox');
+
 const userEMailError = document.getElementById('userEMailError');
+
 const myForm = document.getElementById('myForm');
-// eslint-disable-next-line no-unused-vars
+
 const userPasswordError = document.getElementById('userPasswordError');
-function validate(e) {
+
+const validate = (e) => {
+  
   if (userEmailTextBox.value.includes('admin')) {
-    window.location.href = './admin/addstaff.html';
+   
+    window.location.href = './admin/view_accounts.html';
+
   } else if (userEmailTextBox.value.includes('@banka.com') && !userEmailTextBox.value.includes('admin')) {
+    
     window.location.href = './Cashier/cashtransaction.html';
+  
   } else {
+    
     window.location.href = './Client/dashboard.html';
+  
   }
+  
   e.preventDefault();
-}
+};
+
 myForm.addEventListener('submit', validate);
