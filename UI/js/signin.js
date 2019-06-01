3e65022dfd68a62276d7a56c21eba10927642cec
Iyara Ferguson <fergusoniyara@gmail.com>
2019-06-01 14:31:26 +0100

    chore(refactoring): Refactor signin.js and properly indent signin.html [Delivers #166394986]

--- UI/js/signin.js
+++ UI/js/signin.js
@@ -2,16 +2,43 @@ const userEmailTextBox = document.getElementById('userEmailTextBox');
 
 const userPasswordBox = document.getElementById('userPasswordTextBox');
 
-const userEMailError = document.getElementById('userEMailError');
-
 const myForm = document.getElementById('myForm');
 
 const successForm = document.getElementById('successForm');
 
-const userPasswordError = document.getElementById('userPasswordError');
-
 const url = 'https://banka--app.herokuapp.com/api/v2/auth/signin';
 
+const setSessionStorage = (dataSource) => {
+  sessionStorage.setItem('token', dataSource.data.token);
+  sessionStorage.setItem('email', dataSource.data.email);
+  sessionStorage.setItem('id', dataSource.data.id);
+  sessionStorage.setItem('firstName', dataSource.data.firstName);
+  sessionStorage.setItem('lastName', dataSource.data.lastName);
+  localStorage.setItem('email', dataSource.data.email);
+  sessionStorage.setItem('type', dataSource.data.type);
+};
+
+const redirectUserBasedOnRole = (typeOfUser) => {
+  switch (typeOfUser) {
+    case 'client':
+      window.location.href = './Client/dashboard.html';
+      break;
+    case 'cashier':
+      window.location.href = './Cashier/cashtransaction.html';
+      break;
+    case 'admin':
+      window.location.href = './admin/addstaff.html';
+      break;
+    default:
+      successForm.textContent = 'Something has gone wrong, contact admin';
+      break;
+  }
+};
+
+const displayErrorMessage = (dataSource) => {
+  successForm.textContent = dataSource.error;
+  successForm.style.display = 'block';
+};
 
 const validate = (e) => {
   e.preventDefault();
@@ -23,59 +50,18 @@ const validate = (e) => {
     },
     body: JSON.stringify({
       email: userEmailTextBox.value,
-      password: userPasswordTextBox.value,
+      password: userPasswordBox.value,
     }),
   })
     .then(res => res.json())
-    .then((data) => { 
-      if (data.error === 'Your email is required, example fergusoniyara@banka.com') {
-        userEMailError.textContent = data.error;
-        userEMailErrorr.style.display = 'block';
-        userEMailError.style.fontSize = '13px';
-        userPasswordError.style.display = 'none';
-      }
-      if (data.error === 'Password should be at least 4 characters without any whitespace(s)') {
-        userPasswordError.textContent = data.error;
-        userPasswordError.style.display = 'block';
-        userPasswordError.style.fontSize = '13px';
-        userEMailError.style.display = 'none';
-      }
-      if (data.error === 'Email does not exist') {
-        userEMailError.textContent = data.error;
-        userEMailError.style.display = 'block';
-        userEMailError.style.fontSize = '13px';
-        userPasswordError.style.display = 'none';
-      }
-      if (data.error === 'Email or Password Incorrect') {
-        successForm.textContent = data.error;
-        successForm.style.display = 'block';
-        userEMailError.style.display = 'none';
-        userPasswordError.style.display = 'none';
-      }
+    .then((data) => {
+      if (data.error)displayErrorMessage(data);
       if (!data.error) {
-        const { type } = data.data;
-        console.log(data);
-        if (type === 'client') {
-          sessionStorage.setItem('token', data.data.token);
-          sessionStorage.setItem('email', data.data.email);
-          sessionStorage.setItem('id', data.data.id);
-          sessionStorage.setItem('firstName', data.data.firstName);
-          sessionStorage.setItem('lastName', data.data.lastName);
-          localStorage.setItem('email', data.data.email);
-          sessionStorage.setItem('type', data.data.type);
-          sessionStorage.setItem('admin', data.data.isadmin);         
-          window.location.href = './Client/dashboard.html';
-        }
-        if (type === 'cashier') {
-          window.location.href = './Cashier/cashtransaction.html';
-        }
-        if (type === 'admin') {
-          window.location.href = './admin/view_accounts.html';
-        }
+        successForm.style.display = 'none';
+        setSessionStorage(data);
+        redirectUserBasedOnRole(data.data.type);
       }
     });
 };
-
-
 
 myForm.addEventListener('submit', validate);
