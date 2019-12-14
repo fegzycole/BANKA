d0d5364d388caf9ec231fc667f33dae29f4be019
fegzycole <fergusoniyara@gmail.com>
2019-12-14 15:23:21 +0100

    Get recent updates

--- Server/Middleware/passport/authentication.js
+++ Server/Middleware/passport/authentication.js
@@ -3,3 +3,13 @@ import passport from 'passport';
 export const facebookAuth = () => passport.authenticate('facebook');
 
 export const facebookAuthRedirect = () => passport.authenticate('facebook', { session: false });
+
+export const twitterAuth = () => passport.authenticate('twitter');
+
+export const twitterAuthRedirect = () => passport.authenticate('twitter', { session: false });
+
+export const googleAuth = () => passport.authenticate('google', {
+  scope: ['profile', 'email']
+});
+
+export const googleAuthRedirect = () => passport.authenticate('google', { session: false });
--- Server/Middleware/passport/strategies.js
+++ Server/Middleware/passport/strategies.js
@@ -1,11 +1,18 @@
 import FacebookStrategy from 'passport-facebook';
+import TwitterStrategy from 'passport-twitter';
+import GoogleStrategy from 'passport-google-oauth2';
 import socialAuthConfig from '../../config/passport.config';
 import callBackFunction from './callback';
 
-const { facebook } = socialAuthConfig;
+const { facebook, twitter, google } = socialAuthConfig;
 
 const passportMiddleware = (passport, server) => {
   server.use(passport.initialize());
+
   passport.use('facebook', new FacebookStrategy(facebook, callBackFunction));
+
+  passport.use('twitter', new TwitterStrategy(twitter, callBackFunction));
+
+  passport.use('google', new GoogleStrategy(google, callBackFunction));
 };
 export default passportMiddleware;
