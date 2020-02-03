43a52e6f037b498f14e2a91fdeea4bbda7f259b5
fegzycole <fergusoniyara@gmail.com>
2020-02-03 08:10:02 +0100

    clean file up

--- Server/migrations/20191225173519-create-account.js
+++ Server/migrations/20191225173519-create-account.js
@@ -1,48 +1,45 @@
-'use strict';
+
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Accounts', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Accounts', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    accountNumber: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      unique: true,
+    },
+    owner: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Users',
+        key: 'id',
+        as: 'owner',
       },
-      accountNumber: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        unique: true,
-      },
-      owner: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: 'Users',
-          key: 'id',
-          as: 'owner',
-        },
-      },
-      type: {
-        type: Sequelize.ENUM('savings', 'current')
-      },
-      status: {
-        type: Sequelize.ENUM('draft', 'active', 'dormant'),
-        defaultValue: 'draft',
-      },
-      balance: {
-        type: Sequelize.FLOAT
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Accounts');
-  }
+    },
+    type: {
+      type: Sequelize.ENUM('savings', 'current'),
+    },
+    status: {
+      type: Sequelize.ENUM('draft', 'active', 'dormant'),
+      defaultValue: 'draft',
+    },
+    balance: {
+      type: Sequelize.FLOAT,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
+  // eslint-disable-next-line no-unused-vars
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Accounts'),
 };
--- Server/migrations/20191226104132-create-transaction.js
+++ Server/migrations/20191226104132-create-transaction.js
@@ -1,49 +1,46 @@
-'use strict';
+
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Transactions', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Transactions', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    cashier: {
+      type: Sequelize.INTEGER,
+    },
+    type: {
+      type: Sequelize.ENUM('credit', 'debit'),
+    },
+    oldBalance: {
+      type: Sequelize.FLOAT,
+    },
+    newBalance: {
+      type: Sequelize.FLOAT,
+    },
+    amount: {
+      type: Sequelize.FLOAT,
+    },
+    accountNumber: {
+      type: Sequelize.INTEGER,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'Accounts',
+        key: 'accountNumber',
+        as: 'accountNumber',
       },
-      cashier: {
-        type: Sequelize.INTEGER
-      },
-      type: {
-        type: Sequelize.ENUM('credit', 'debit')
-      },
-      oldBalance: {
-        type: Sequelize.FLOAT
-      },
-      newBalance: {
-        type: Sequelize.FLOAT
-      },
-      amount: {
-        type: Sequelize.FLOAT
-      },
-      accountNumber: {
-        type: Sequelize.INTEGER,
-        onDelete: 'CASCADE',
-        references: {
-          model: 'Accounts',
-          key: 'accountNumber',
-          as: 'accountNumber',
-        },
-        allowNull: false
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Transactions');
-  }
+      allowNull: false,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
+  // eslint-disable-next-line no-unused-vars
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Transactions'),
 };
